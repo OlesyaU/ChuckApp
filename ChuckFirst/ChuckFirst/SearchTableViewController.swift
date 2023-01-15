55f5d74e1598cddc670bd75f4923b3979820b9ed
Olesya <53311515+OlesyaU@users.noreply.github.com>
2023-01-15 09:25:58 +0300

    com

--- ChuckFirst/ChuckFirst/SearchTableViewController.swift
+++ ChuckFirst/ChuckFirst/SearchTableViewController.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 class SearchTableViewController: UITableViewController, UISearchBarDelegate {
-
+    
     var jokesArray: [String] = []
-     var searchwindow = UISearchController()
+    var searchwindow = UISearchController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.searchController = searchwindow
         searchwindow.searchBar.delegate = self
-}
+    }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
         guard let searchText = searchBar.text, searchText.count >= 3  else {
@@ -26,31 +26,29 @@ class SearchTableViewController: UITableViewController, UISearchBarDelegate {
             return
         }
         
-        
-        
-            Model().downloadJokesList(queryText: searchText) { [weak self] jokesArray in
-                DispatchQueue.main.async {
-                    let joAR = jokesArray ?? []
-                    if joAR.count  > 0 {
-                        self?.jokesArray = jokesArray ?? []
-                    } else {
-                        self?.jokesArray = ["EMMPPTTYYNNEESS....try another word"]
-                    }
+        Model().downloadJokesList(queryText: searchText) { [weak self] jokesArray in
+            DispatchQueue.main.async {
+                let joAR = jokesArray ?? []
+                if joAR.count  > 0 {
+                    self?.jokesArray = jokesArray ?? []
+                } else {
+                    self?.jokesArray = ["EMMPPTTYYNNEESS....try another word"]
+                }
                 self?.tableView.reloadData()
             }
         }
     }
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
+        
         return jokesArray.count
     }
-
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let jokeText = jokesArray[indexPath.row]
         cell.textLabel?.text = jokeText
         return cell
@@ -60,8 +58,8 @@ class SearchTableViewController: UITableViewController, UISearchBarDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
         guard let textJoke = tableView.cellForRow(at: indexPath)?.textLabel?.text else { print("text joke is emty")
             return
-            
         }
+        
         var activityControoller = UIActivityViewController(activityItems: [textJoke], applicationActivities: nil)
         present(activityControoller, animated: true)
     }
